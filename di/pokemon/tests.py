1a8f5937c8ff35a53e0ff2537d42f7646b83a677
Jésica Collado <collado.jesica@gmail.com>
2021-06-01 23:28:19 -0300

    Add API call to know if a pokemon can learn a move

--- di/pokemon/tests.py
+++ di/pokemon/tests.py
@@ -13,3 +13,11 @@ class CanLearnTestCase(TestCase):
         data = json.loads(response.content)
 
         self.assertTrue(data['can_learn'])
+
+    def test_squirtle_cannot_learn_ember(self):
+        request = RequestFactory().get('/pokemon/squirtle/can_learn', {'move': 'ember'})
+
+        response = can_learn(request, 'squirtle')
+        data = json.loads(response.content)
+
+        self.assertFalse(data['can_learn'])
--- di/pokemon/views.py
+++ di/pokemon/views.py
@@ -1,5 +1,19 @@
+import requests
+
 from django.http import JsonResponse
 
 
 def can_learn(request, pokemon):
-    return JsonResponse({'can_learn': True})
+    move_name = request.GET.get('move')
+
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/squirtle')
+    data = response.json()
+
+    for move in data['moves']:
+        if move['move']['name'] == move_name:
+            can_learn = True
+            break
+    else:
+        can_learn = False
+
+    return JsonResponse({'can_learn': can_learn})
